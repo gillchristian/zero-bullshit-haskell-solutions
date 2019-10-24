1a09745fd85d6bb20f8272cfcfdb88d4c96a5ffe
gillchristian <gillchristiang@gmail.com>
2019-10-24 19:14:05 +0200

    Exercise 03

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,15 +3,24 @@ module Lib where
 import qualified Zero.Server as Server
 
 helloHandler :: Server.Request -> Server.Response
-helloHandler _
-  = Server.stringResponse "hello"
+helloHandler _ =
+  Server.stringResponse "hello"
 
 echoHandler :: Server.Request -> Server.Response
 echoHandler = Server.stringResponse . Server.requestBody
 
+caseHandler :: Server.Request -> Server.Response
+caseHandler req =
+  Server.stringResponse $ case Server.requestBody req of
+    "1" -> "one"
+    "2" -> "two"
+    "3" -> "three"
+    _ -> "What am I, a mathematician?"
+
 run :: IO ()
-run
-  = Server.startServer
-      [ Server.simpleHandler Server.GET "/hello" helloHandler
-      , Server.simpleHandler Server.POST "/echo" echoHandler
-      ]
+run =
+  Server.startServer
+    [ Server.simpleHandler Server.GET "/hello" helloHandler,
+      Server.simpleHandler Server.POST "/echo" echoHandler,
+      Server.simpleHandler Server.POST "/case" caseHandler
+    ]
