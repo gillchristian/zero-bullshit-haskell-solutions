f191ed6992c7a79cb1fb4b5b72c0eba4e2f9993e
gillchristian <gillchristiang@gmail.com>
2019-10-24 19:40:37 +0200

    Exercise 06

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,12 +3,18 @@ module Lib where
 import qualified Data.List as List
 import qualified Zero.Server as Server
 
+-- Exercise 01
+
 helloHandler :: Server.Request -> Server.Response
 helloHandler _ =
   Server.stringResponse "hello"
 
+-- Exercise 02
+
 echoHandler :: Server.Request -> Server.Response
 echoHandler = Server.stringResponse . Server.requestBody
+
+-- Exercise 03
 
 caseHandler :: Server.Request -> Server.Response
 caseHandler req =
@@ -18,6 +24,8 @@ caseHandler req =
     "3" -> "three"
     _ -> "What am I, a mathematician?"
 
+-- Exercise 04
+
 stringManipulationHandler :: Server.Request -> Server.Response
 stringManipulationHandler req =
   Server.stringResponse $ List.intercalate " " $
@@ -25,11 +33,23 @@ stringManipulationHandler req =
       ("I'm" : "positive" : s) -> ("I" : "think" : s)
       s -> s
 
+-- Exercise 05
+
 data State = On | Off deriving (Eq, Show)
 
 onOffSwitchHandler :: State -> Server.Request -> (State, Server.Response)
 onOffSwitchHandler On _ = (Off, Server.stringResponse $ show On)
 onOffSwitchHandler Off _ = (On, Server.stringResponse $ show Off)
+
+-- Exercise 06
+
+currentCountHandler :: Int -> Server.Request -> (Int, Server.Response)
+currentCountHandler n _ = (n, Server.stringResponse $ show n)
+
+increaseHandler :: Int -> Server.Request -> (Int, Server.Response)
+increaseHandler n _ = (n + 1, Server.stringResponse "")
+
+-- Server
 
 run :: IO ()
 run =
@@ -40,5 +60,10 @@ run =
       Server.simpleHandler Server.POST "/string-manipulation" stringManipulationHandler,
       Server.handlersWithState
         On
-        [Server.statefulHandler Server.POST "/onoff-switch" onOffSwitchHandler]
+        [Server.statefulHandler Server.POST "/onoff-switch" onOffSwitchHandler],
+      Server.handlersWithState
+        0
+        [ Server.statefulHandler Server.GET "/current-count" currentCountHandler,
+          Server.statefulHandler Server.POST "/increase" increaseHandler
+        ]
     ]
